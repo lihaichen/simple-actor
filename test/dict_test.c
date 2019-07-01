dcee40ce84fd2e5c8ee1b479e58dc826254c4581
lihaichen <li8303@163.com>
2019-07-01 19:29:49 +0800

    add string key test

--- test/dict_test.c
+++ test/dict_test.c
@@ -1,5 +1,6 @@
 #include <criterion/criterion.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include "actor_dict.h"
@@ -15,15 +16,54 @@ static unsigned int hash_int(const void* key) {
   return murmurhash(buf, sizeof(int), 0);
 }
 
-actor_dict_type_t type1 = {.hash = hash_int,
-                           .key_compare = NULL,
-                           .key_dup = NULL,
-                           .val_dup = NULL,
-                           .key_delete = NULL,
-                           .val_delete = NULL};
+static actor_dict_type_t type_int = {.hash = hash_int,
+                                     .key_compare = NULL,
+                                     .key_dup = NULL,
+                                     .val_dup = NULL,
+                                     .key_delete = NULL,
+                                     .val_delete = NULL};
 
-Test(actor_dict, dict_simple) {
-  actor_dict_t* dict = actor_dict_create(&type1);
+static unsigned int hash_string(const void* key) {
+  return murmurhash(key, strlen(key), 0);
+}
+
+static int key_compare_string(const void* key1, const void* key2) {
+  return strcmp(key1, key2) == 0 ? 1 : 0;
+}
+
+static void* key_dup_string(const void* key) {
+  int len = strlen(key) + 1;
+  void* res = malloc(len);
+  memset(res, 0, len);
+  strcpy(res, key);
+  return res;
+}
+
+static void* val_dup_string(const void* val) {
+  int len = strlen(val) + 1;
+  void* res = malloc(len);
+  memset(res, 0, len);
+  strcpy(res, val);
+  return res;
+}
+
+static void key_delete_string(void* key) {
+  free(key);
+}
+
+static void val_delete_string(void* val) {
+  free(val);
+}
+
+static actor_dict_type_t type_string = {.hash = hash_string,
+                                        .key_compare = key_compare_string,
+                                        .key_dup = key_dup_string,
+                                        .val_dup = val_dup_string,
+                                        .key_delete = key_delete_string,
+                                        .val_delete = val_delete_string};
+
+Test(actor_dict, dict_int_simple) {
+  actor_dict_t* dict = actor_dict_create(&type_int);
   cr_expect_not_null(dict, "actor_dict_create null");
   long key = 0x2;
   int ret = actor_dict_add(dict, (void*)key, NULL);
@@ -42,8 +82,38 @@ Test(actor_dict, dict_simple) {
   return;
 }
 
+Test(actor_dict, dict_string_size) {
+  actor_dict_t* dict = actor_dict_create(&type_string);
+  cr_expect_not_null(dict, "actor_dict_create null");
+  char buf[64] = {0};
+
+  for (int i = 0; i < HASH_SIZE; i++) {
+    memset(buf, 0, sizeof(buf));
+    sprintf(buf, "hello%03d", i);
+    actor_dict_add(dict, (void*)buf, buf);
+  }
+  cr_expect_eq(HASH_SIZE, dict->ht[0].used, "used not match");
+  for (int i = 0; i < HASH_SIZE; i++) {
+    memset(buf, 0, sizeof(buf));
+    sprintf(buf, "hello%03d", i);
+    actor_dict_entry_t* entry = actor_dict_find(dict, (void*)buf);
+    cr_expect_not_null(entry, "actor_dict_find null");
+    cr_expect_str_eq(buf, entry->key, "actor_dict_find key not eq");
+    actor_dict_delete(dict, (void*)buf);
+  }
+  cr_expect_eq(0, dict->ht[0].used, "used not match");
+  for (int i = 0; i < HASH_SIZE; i++) {
+    memset(buf, 0, sizeof(buf));
+    sprintf(buf, "hello%03d", i);
+    actor_dict_entry_t* entry = actor_dict_find(dict, (void*)buf);
+    cr_expect_null(entry, "actor_dict_find must null");
+  }
+  actor_dict_destroy(dict);
+  return;
+}
+
 Test(actor_dict, dict_size) {
-  actor_dict_t* dict = actor_dict_create(&type1);
+  actor_dict_t* dict = actor_dict_create(&type_int);
   cr_expect_not_null(dict, "actor_dict_create null");
   time_t t = time(NULL);
   srand(t);
@@ -73,7 +143,7 @@ Test(actor_dict, dict_size) {
 }
 
 Test(actor_dict, iterator) {
-  actor_dict_t* dict = actor_dict_create(&type1);
+  actor_dict_t* dict = actor_dict_create(&type_int);
   cr_expect_not_null(dict, "actor_dict_create null");
   long buf[] = {0x1, 0x2, 0x2, 0x3, 0x2, 0x5, 0x3, 0x1, 0x4, 0x5, 0x1, 0x3};
   for (int i = 0; i < sizeof(buf) / sizeof(long); i++) {
